c0cc3cdb89c7f456787c676a85578c96e844670b
Ian Cook <ianscook19@gmail.com>
2023-01-01 21:25:26 -0500

    using hashrouter instead

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
       <Home/>
-    </BrowserRouter>
+    </HashRouter>
 )
